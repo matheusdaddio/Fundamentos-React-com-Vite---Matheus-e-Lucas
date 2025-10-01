d63567bc39503130b164c434f6ede6348b6bb63a
Daddio <matheus.daddio12@gmail.com>
2025-10-01 10:49:11 -0300

    Adicionando o Events

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // src/App.jsx
 import './App.css'
-import FirstComponent from './components/FirstComponent' // LINHA ADICIONADA
+import FirstComponent from './components/FisrtComponent'
 import TemplateExpressions from './components/TemplateExpressions' // LINHA ADICIONADA
+import Events from './components/Events' // LINHA ADICIONADA
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
-      <FirstComponent /> // LINHA ADICIONADA
+      <FirstComponent /> {/* LINHA ADICIONADA */}
       <TemplateExpressions /> {/* LINHA ADICIONADA */}
+      <Events /> {/* LINHA ADICIONADA */}
     </div>
   )
 }
